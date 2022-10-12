4b414f16070f75790d354a9d1b60da10340017e7
anlexbes <114681208+anlexbes@users.noreply.github.com>
2022-10-12 15:16:54 +0000

    Initial commit

---
+++ commands/info/invite.js
@@ -0,0 +1,50 @@
+const ButtonPages = require('discord-button-pages');
+const { MessageEmbed } = require('discord.js')
+const disbutpages = require("discord-embeds-pages-buttons")
+const Discord = require("discord.js");
+const disbut = require("discord-buttons");
+const MessageButton = require("discord-buttons");
+
+module.exports = {
+  name: "invite",
+  aliases: ["info"],
+  description: "Shows all commands of the bot",
+
+  run: async (client, message, args) => {
+    let helpEmbed = new MessageEmbed()
+      .setTitle("THX FOR INVITING")
+      .setDescription("I am a all in one discord bot ")
+      .setImage('https://cdn.discordapp.com/attachments/890116247822434355/890535152168607744/standard_45.gif')
+      .setThumbnail(client.user.displayAvatarURL())
+      .setFooter("Click on these buttons to know more about me!")
+      .setColor("#FFFFF0")
+
+
+      let button1 = new disbut.MessageButton()
+      .setStyle('url')
+      .setLabel(' Invite Me')
+      .setEmoji('889814501506056202') 
+      .setURL("https://discord.com/oauth2/authorize?client_id=864067337628155905&permissions=3275895&scope=bot")
+
+      let button2 = new disbut.MessageButton()
+      .setStyle('url')
+      .setLabel(' Support Server') 
+      .setEmoji(`889820978358681610`)
+      .setURL("https://discord.gg/2pGT7qPWVA")
+
+      let button3 = new disbut.MessageButton()
+      .setStyle('url')
+      .setLabel('DEVLOPERS + SOURCE') 
+      .setEmoji(`889822366945914890`)
+      .setURL("https://www.youtube.com/channel/UCzAGQrMCsPo-NOqfFUW0Kjg")
+       let button4 = new disbut.MessageButton()
+      .setStyle('gray')
+      .setID('button1')
+      .setLabel('DEVLOPERS - ϏȢ ✦DEVIL NISHANT#7627 And  Eagle々Team-Devil#0070') 
+      .setDisabled(true)
+      return message.channel.send(helpEmbed,{
+        button: [button1,button2,button3,button4],
+      });
+
+  },
+};
